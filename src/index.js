713d1fc3b15d66eba22e421713f5ce2dcad9496d
Lancelot <bincha.1997@gmail.com>
2019-07-19 19:09:01 +1000

    Added header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.sass'
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMobileMenuBtn = document.getElementById('mobile-menu-close');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('active');
+})
+
+closeMobileMenuBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    mobileMenu.classList.remove('active');
+})
