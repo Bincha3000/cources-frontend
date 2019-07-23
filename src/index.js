5d32e12a6b7317adb4f0cf2b912fb6b03389c1c2
Lancelot <bincha.1997@gmail.com>
2019-07-23 23:45:13 +1000

    Added courses page and js behavior

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import './style.sass'
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeMobileMenuBtn = document.getElementById('mobile-menu-close');
+const scrollTop = document.getElementById('scroll-up')
 
 menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('active');
+  mobileMenu.classList.add('active');
 })
 
 closeMobileMenuBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    mobileMenu.classList.remove('active');
+  event.preventDefault();
+  mobileMenu.classList.remove('active');
 })
+
+scrollTop.onclick = function () {
+  window.scrollTo(pageXOffset, 0);
+};
+
+window.addEventListener('scroll', function () {
+  scrollTop.style.display = pageYOffset < document.documentElement.clientHeight ? 'none' : 'block';
+});
