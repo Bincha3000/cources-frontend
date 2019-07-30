4949e9f4bf3e4f3aaaaa297b8490a51ffa89ad3f
Lancelot <bincha.1997@gmail.com>
2019-07-30 16:35:10 +1000

    Added new page timetable

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ const mobileMenu = document.getElementById('mobile-menu');
 const closeMobileMenuBtn = document.getElementById('mobile-menu-close');
 const scrollTop = document.getElementById('scroll-up')
 
-menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.add('active');
-})
+menuBtn.addEventListener('click', () => mobileMenu.classList.add('active'));
 
 closeMobileMenuBtn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -21,3 +19,18 @@ scrollTop.onclick = function () {
 window.addEventListener('scroll', function () {
   scrollTop.style.display = pageYOffset < document.documentElement.clientHeight ? 'none' : 'block';
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
